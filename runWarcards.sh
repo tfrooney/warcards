6a6064fbda72ea7fb0a1d906daaf09e90d5ece93
Thomas Rooney <trooney@TheStudio.local>
2015-12-02 07:58:28 -0600

    added python testing to shell script

--- runWarcards.sh
+++ runWarcards.sh
@@ -6,3 +6,10 @@ echo "Test Invalid Command Line"
 java -jar warcards.jar
 echo "Test Error input"
 java -jar warcards.jar A B
+
+echo "Python version of warcards"
+echo " 2 players, warpile 3"
+python warcards.py 2 3
+echo " 4 players, warpile 1"
+python warcards.py 4 1
+
