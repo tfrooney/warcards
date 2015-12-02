9b060597d66d4a5c0d65752bb7e539c1434b5bb6
Thomas Rooney <trooney@TheStudio.local>
2015-12-01 19:14:09 -0600

    added script to run test cases

---
+++ runWarcards.sh
@@ -0,0 +1,8 @@
+echo "Test warcards 2 Players, Warpile 3"
+java -jar warcards.jar 2 3
+echo "Test warcards 4 Players, Warpile 3"
+java -jar warcards.jar 4 3
+echo "Test Invalid Command Line"
+java -jar warcards.jar
+echo "Test Error input"
+java -jar warcards.jar A B
